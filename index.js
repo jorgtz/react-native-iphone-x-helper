c5165f03580bd99a8861ef8d7d61c510d6b2f93a
Jor <66479719+jorgtz@users.noreply.github.com>
2022-09-20 16:14:44 -0500

    Fix

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ export function isIphoneX() {
         Platform.OS === 'ios' &&
         !Platform.isPad &&
         !Platform.isTVOS &&
-        (
-             (dimen.height === 780 || dimen.width === 780)
+        (    (dimen.height === 780 || dimen.width === 780)
           || (dimen.height === 812 || dimen.width === 812)
           || (dimen.height === 844 || dimen.width === 844)
           || (dimen.height === 896 || dimen.width === 896)
           || (dimen.height === 926 || dimen.width === 428)
           || (dimen.height === 852 || dimen.width === 852) // 14 Pro
           || (dimen.height === 932 || dimen.width === 932) // 14 Pro Max
-    );
+    ));
 }
 
 export function hasIsland() {
